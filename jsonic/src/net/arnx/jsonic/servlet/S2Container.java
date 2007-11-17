bb1a1077ba9a96b4bf1a54359c7b58c5cbe27063
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2007-11-17 17:31:11 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@106 a27f3287-6b31-0410-ac9d-9e0373bb86a5

--- jsonic/src/net/arnx/jsonic/servlet/S2Container.java
+++ jsonic/src/net/arnx/jsonic/servlet/S2Container.java
@@ -1,26 +1,54 @@
 package net.arnx.jsonic.servlet;
 
+import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
+import org.seasar.framework.env.Env;
+import org.seasar.framework.log.Logger;
+import org.seasar.framework.util.StringUtil;
+import org.seasar.framework.convention.NamingConvention;
+
 public class S2Container implements Container {
+	private org.seasar.framework.container.S2Container container;
+	private Logger logger = Logger.getLogger(S2Container.class);
+	private NamingConvention nc;
+	
 	@Override
 	public void init() {
+		container = SingletonS2ContainerFactory.getContainer();
+		nc = (NamingConvention)container.getComponent(NamingConvention.class);
+	}
+
+	@Override
+	public Object getComponent(String path) throws Exception {
+		return container.getComponent(fromPathToConmponentName(path, nc.getServiceSuffix()));
+	}
+
+	@Override
+	public boolean isDebugMode() {
+		return Env.UT.equals(Env.getValue());
+	}
+
+	@Override
+	public void log(String message, Throwable e) {
+		logger.log(message, null, e);
 	}
 	
 	@Override
 	public void destory() {
 	}
-
-	@Override
-	public Object getComponent(String path) throws Exception {
-		return null;
-	}
-
-	@Override
-	public boolean isDebugMode() {
-		return false;
-	}
-
-	@Override
-	public void log(String message, Throwable e) {
-	}
-
+	
+    protected String fromPathToConmponentName(String path, String nameSuffix) {
+        if (!path.startsWith(nc.getViewRootPath()) || !path.endsWith(nc.getViewExtension())) {
+            throw new IllegalArgumentException(path);
+        }
+        String componentName = (path.substring(
+                nc.adjustViewRootPath().length() + 1, path.length()
+                        - nc.getViewExtension().length()) + nameSuffix).replace('/',
+                '_');
+        int pos = componentName.lastIndexOf('_');
+        if (pos == -1) {
+            return StringUtil.decapitalize(componentName);
+        }
+        return componentName.substring(0, pos + 1)
+        	+ StringUtil.decapitalize(componentName.substring(pos + 1));
+    }
 }
