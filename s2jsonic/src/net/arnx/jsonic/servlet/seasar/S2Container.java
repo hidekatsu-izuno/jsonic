d814acdf2a63363aaef729a8caf32dc015f223df
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2007-11-18 05:45:54 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@127 a27f3287-6b31-0410-ac9d-9e0373bb86a5

---
+++ s2jsonic/src/net/arnx/jsonic/servlet/seasar/S2Container.java
@@ -0,0 +1,60 @@
+package net.arnx.jsonic.servlet.seasar;
+
+import net.arnx.jsonic.servlet.Container;
+
+import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
+import org.seasar.framework.env.Env;
+import org.seasar.framework.log.Logger;
+import org.seasar.framework.util.StringUtil;
+import org.seasar.framework.convention.NamingConvention;
+
+public class S2Container implements Container {
+	private org.seasar.framework.container.S2Container container;
+	private Logger logger = Logger.getLogger(S2Container.class);
+	
+	@Override
+	public void init() {
+		container = SingletonS2ContainerFactory.getContainer();
+	}
+
+	@Override
+	public Object getComponent(String path) throws Exception {
+		return container.getComponent(fromPathToConmponentName(path));
+	}
+
+	@Override
+	public boolean isDebugMode() {
+		return Env.UT.equals(Env.getValue());
+	}
+
+	@Override
+	public void log(String message, Throwable e) {
+		if (e != null) {
+			logger.log(message, null, e);
+		} else {
+			logger.log(message, null);
+		}
+	}
+	
+	@Override
+	public void destory() {
+	}
+	
+    protected String fromPathToConmponentName(String path) {
+    	NamingConvention nc = (NamingConvention)container.getComponent(NamingConvention.class);
+    	String nameSuffix = nc.getServiceSuffix();
+        if (!path.startsWith(nc.getViewRootPath()) || !path.endsWith(nc.getViewExtension())) {
+            throw new IllegalArgumentException(path);
+        }
+        String componentName = (path.substring(
+                nc.adjustViewRootPath().length() + 1, path.length()
+                        - nc.getViewExtension().length()) + nameSuffix).replace('/',
+                '_');
+        int pos = componentName.lastIndexOf('_');
+        if (pos == -1) {
+            return StringUtil.decapitalize(componentName);
+        }
+        return componentName.substring(0, pos + 1)
+        	+ StringUtil.decapitalize(componentName.substring(pos + 1));
+    }
+}
