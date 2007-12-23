462dda51ea792a3b5f74dc94cfb1852d505bbe54
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2007-12-23 13:24:54 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@177 a27f3287-6b31-0410-ac9d-9e0373bb86a5

--- s2jsonic/src/net/arnx/jsonic/servlet/seasar/S2Container.java
+++ s2jsonic/src/net/arnx/jsonic/servlet/seasar/S2Container.java
@@ -2,36 +2,25 @@ package net.arnx.jsonic.servlet.seasar;
 
 import javax.servlet.ServletContext;
 
-import net.arnx.jsonic.servlet.Container;
+import net.arnx.jsonic.container.Container;
 
 import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
 import org.seasar.framework.env.Env;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.seasar.framework.util.StringUtil;
 
 public class S2Container implements Container {
 	private Log logger = LogFactory.getLog(S2Container.class);
 	
 	public Boolean debug;
-	public String encoding = "UTF-8";
-	public ServiceConfig service;
 	
 	public void init(ServletContext context) {
-		if (service == null) service = new ServiceConfig();
-		if (service.rootPath == null) service.rootPath = "";
-		if (service.suffix == null) service.suffix = "Service";
-		if (service.extension == null) service.extension = ".json";
-	}
-	
-	public String getCharacterEncoding() {
-		return encoding;
 	}
 
-	public Object getComponent(String path) throws Exception {
+	public Object getComponent(Class c) throws Exception {
 		return SingletonS2ContainerFactory
 			.getContainer()
-			.getComponent(fromPathToConmponentName(path));
+			.getComponent(c);
 	}
 
 	public boolean isDebugMode() {
@@ -47,30 +36,5 @@ public class S2Container implements Container {
 	}
 	
 	public void destory() {
-	}
-	
-    protected String fromPathToConmponentName(String path) {
-        if (!path.startsWith(service.rootPath) || !path.endsWith(service.extension)) {
-            throw new IllegalArgumentException(path);
-        }
-        
-        String componentName = path.substring(
-        		("/".equals(service.rootPath) ? "" : service.rootPath).length() + 1,
-                path.length() - service.extension.length()
-            ).replace('/','_') + service.suffix;
-        
-        int pos = componentName.lastIndexOf('_');
-        
-        if (pos == -1) {
-            return StringUtil.decapitalize(componentName);
-        }
-        return componentName.substring(0, pos + 1)
-        	+ StringUtil.decapitalize(componentName.substring(pos + 1));
-    }
-    
-    class ServiceConfig {
-    	public String rootPath;
-    	public String suffix;
-    	public String extension;
     }
 }
