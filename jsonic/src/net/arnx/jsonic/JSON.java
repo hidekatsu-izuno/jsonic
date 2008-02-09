3c918da83a981d1257b55369d4636f1e9ecc8c0c
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2008-02-09 02:45:41 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@290 a27f3287-6b31-0410-ac9d-9e0373bb86a5

--- jsonic/src/net/arnx/jsonic/JSON.java
+++ jsonic/src/net/arnx/jsonic/JSON.java
@@ -1577,14 +1577,14 @@ public class JSON {
 			
 			if (data == null && (c.isPrimitive() || value != null)) {
 				if (!handleConvertError(key, value, c, type, null)) {
-					throw new JSONConvertException(getMessage("json.convert.ConversionError", key, value, type));
+					throw new JSONConvertException(getMessage("json.convert.ConversionError", key, type, value));
 				}
 			}
 		} catch (JSONConvertException e) {
 			throw e;
 		} catch (Exception e) {
 			if (!handleConvertError(key, value, c, type, e)) {
-				throw new JSONConvertException(getMessage("json.convert.ConversionError", key, value, type), e);
+				throw new JSONConvertException(getMessage("json.convert.ConversionError", key, type, value), e);
 			}
 		}
 		
