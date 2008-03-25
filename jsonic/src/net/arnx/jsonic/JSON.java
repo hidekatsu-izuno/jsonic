977e2894fcd22e381a9ba2d8bdd07ae60f354cc6
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2008-03-25 14:13:11 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@407 a27f3287-6b31-0410-ac9d-9e0373bb86a5

--- jsonic/src/net/arnx/jsonic/JSON.java
+++ jsonic/src/net/arnx/jsonic/JSON.java
@@ -1385,8 +1385,8 @@ public class JSON {
 			} else if (byte.class.equals(c) || Byte.class.equals(c)) {
 				if (value instanceof Boolean) {
 					data = (((Boolean)value).booleanValue()) ? 1 : 0;
-				} else if (value instanceof Number) {
-					data = ((Number)value).byteValue();
+				} else if (value instanceof BigDecimal) {
+					data = ((BigDecimal)value).byteValueExact();
 				} else {
 					String str = value.toString().trim();
 					if (str.length() > 0) {
@@ -1398,8 +1398,8 @@ public class JSON {
 			} else if (short.class.equals(c) || Short.class.equals(c)) {
 				if (value instanceof Boolean) {
 					data = (((Boolean)value).booleanValue()) ? 1 : 0;
-				} else if (value instanceof Number) {
-					data = ((Number)value).shortValue();
+				} else if (value instanceof BigDecimal) {
+					data = ((BigDecimal)value).shortValueExact();
 				} else {
 					String str = value.toString().trim();
 					if (str.length() > 0) {
@@ -1411,8 +1411,8 @@ public class JSON {
 			} else if (int.class.equals(c) || Integer.class.equals(c)) {
 				if (value instanceof Boolean) {
 					data = (((Boolean)value).booleanValue()) ? 1 : 0;
-				} else if (value instanceof Number) {
-					data = ((Number)value).intValue();
+				} else if (value instanceof BigDecimal) {
+					data = ((BigDecimal)value).intValueExact();
 				} else {
 					String str = value.toString().trim();
 					if (str.length() > 0) {
@@ -1424,8 +1424,8 @@ public class JSON {
 			} else if (long.class.equals(c) || Long.class.equals(c)) {
 				if (value instanceof Boolean) {
 					data = (((Boolean)value).booleanValue()) ? 1l : 0l;
-				} else if (value instanceof Number) {
-					data = ((Number)value).longValue();
+				} else if (value instanceof BigDecimal) {
+					data = ((BigDecimal)value).longValueExact();
 				} else {
 					String str = value.toString().trim();
 					if (str.length() > 0) {
