530d4d08b3bbabfa0a9d49d03a23dd5cb0925e72
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2008-03-29 20:30:00 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@417 a27f3287-6b31-0410-ac9d-9e0373bb86a5

--- jsonic/sample/WEB-INF/src/sample/web/rest/service/MemoService.java
+++ jsonic/sample/WEB-INF/src/sample/web/rest/service/MemoService.java
@@ -1,30 +1,16 @@
 package sample.web.rest.service;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpSession;
-
 public class MemoService {
 	
-	// auto binding
-	public HttpSession session;
-	
-	// it's incorrect program for sample. you should use RDBMS.
+	// it's incorrect use. you shoud use RDBMS.
 	private static int count = 0;
-	private static Map<Integer, Memo> list;
+	private static Map<Integer, Memo> list = new LinkedHashMap<Integer, Memo>();
 	
 	public void init() {
-		Map map = (Map)session.getAttribute(this.getClass().getName());
-		if (map == null) {
-			count = 0;
-			list = new LinkedHashMap<Integer, Memo>();
-		} else {
-			count = (Integer)map.get("count");
-			list = (Map<Integer, Memo>)map.get("list");
-		}
 	}
 	
 	public Collection<Memo> find() {
@@ -47,16 +33,10 @@ public class MemoService {
 	}
 	
 	public void destroy() {
-		Map map = new HashMap();
-		map.put("count", count);
-		map.put("list", list);
-		session.setAttribute(this.getClass().getName(), map);
 	}
 }
 
-class Memo implements java.io.Serializable {
-	private static final long serialVersionUID = 1L;
-	
+class Memo {
 	public Integer id;
 	public String title;
 	public String text;
