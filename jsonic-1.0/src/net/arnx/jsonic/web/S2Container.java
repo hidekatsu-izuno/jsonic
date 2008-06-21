8430d54d2fe55a1bbb227af63e7dbe2f129873e6
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2008-06-21 16:48:29 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@495 a27f3287-6b31-0410-ac9d-9e0373bb86a5

---
+++ jsonic-1.0/src/net/arnx/jsonic/web/S2Container.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2007-2008 Hidekatsu Izuno
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package net.arnx.jsonic.web;
+
+import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
+import org.seasar.framework.env.Env;
+import org.seasar.framework.log.Logger;
+
+@SuppressWarnings("unchecked")
+public class S2Container extends Container {
+	private static Logger log = Logger.getLogger(S2Container.class);
+	
+	@Override
+	public <T> T getComponent(Class<? extends T> c) throws Exception {
+		return (T)SingletonS2ContainerFactory
+			.getContainer()
+			.getComponent(c);
+	}
+	
+	@Override
+	public boolean isDebugMode() {
+		return (debug != null) ? debug : Env.UT.equals(Env.getValue());
+	}
+
+	@Override
+	public void debug(String message) {
+		log.debug(message);
+	}
+
+	@Override
+	public void error(String message, Throwable e) {
+		log.error(message, e);
+	}
+}
