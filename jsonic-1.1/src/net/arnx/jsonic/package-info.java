e26770c3c564c15c1a55c506d9086b0b2adbef18
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2009-02-01 07:43:37 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@827 a27f3287-6b31-0410-ac9d-9e0373bb86a5

--- jsonic-1.1/src/net/arnx/jsonic/package-info.java
+++ jsonic-1.1/src/net/arnx/jsonic/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2008 Hidekatsu Izuno
+ * Copyright 2007-2009 Hidekatsu Izuno
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
