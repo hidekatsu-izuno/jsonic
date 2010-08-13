287aa752eb4a63d91a76f2da425b52b3cab21849
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2010-08-13 12:42:29 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@1192 a27f3287-6b31-0410-ac9d-9e0373bb86a5

--- jsonic-1.2/src/net/arnx/jsonic/JSON.java
+++ jsonic-1.2/src/net/arnx/jsonic/JSON.java
@@ -163,11 +163,14 @@ import org.w3c.dom.NodeList;
  * </table>
  * 
  * @author Hidekatsu Izuno
- * @version 1.2.0
+ * @version 1.2.1
  * @see <a href="http://www.rfc-editor.org/rfc/rfc4627.txt">RFC 4627</a>
  * @see <a href="http://www.apache.org/licenses/LICENSE-2.0">the Apache License, Version 2.0</a>
  */
 public class JSON {
+	/**
+	 * JSON process mode
+	 */
 	public enum Mode {
 		TRADITIONAL,
 		STRICT,
