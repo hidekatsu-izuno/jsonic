a512d6b1a3b0843595407516bdfe59dc35e25099
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2010-10-18 15:43:12 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@1246 a27f3287-6b31-0410-ac9d-9e0373bb86a5

---
+++ jsonic-1.2/test/net/arnx/jsonic/ClassUtilTest.java
@@ -0,0 +1,29 @@
+package net.arnx.jsonic;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ClassUtilTest {
+	@Test
+	public void testFindClass() throws Exception {
+		URLClassLoader cl = new URLClassLoader(new URL[] { new File("./lib/commmons-logging.jar").toURI().toURL() });
+		
+		ClassLoader current = Thread.currentThread().getContextClassLoader();
+		Thread.currentThread().setContextClassLoader(cl);
+		
+		assertNotNull(ClassUtil.findClass("org.apache.commons.logging.Log"));
+		
+		Thread.currentThread().setContextClassLoader(current);
+		cl = null;
+		
+		System.gc();
+		
+		Thread.sleep(1000);
+
+		assertTrue(ClassUtil.cache.isEmpty());
+	}
+}
