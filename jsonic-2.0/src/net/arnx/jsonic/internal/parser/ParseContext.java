12b77de2a8f2952d6622e06264c69463180969e9
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2012-01-22 16:31:23 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@1545 a27f3287-6b31-0410-ac9d-9e0373bb86a5

--- jsonic-2.0/src/net/arnx/jsonic/internal/parser/ParseContext.java
+++ jsonic-2.0/src/net/arnx/jsonic/internal/parser/ParseContext.java
@@ -1,5 +1,7 @@
 package net.arnx.jsonic.internal.parser;
 
+import java.io.IOException;
+import java.math.BigDecimal;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +14,17 @@ import net.arnx.jsonic.internal.io.StringCache;
 
 class ParseContext {
 	public static final Object EMPTY = new Object();
+	private static final int[] ESCAPE_CHARS = new int[128];
+	
+	static {
+		for (int i = 0; i < 32; i++) {
+			ESCAPE_CHARS[i] = 1;
+		}
+		ESCAPE_CHARS['\\'] = 2;
+		ESCAPE_CHARS['"'] = 3;
+		ESCAPE_CHARS['\''] = 3;
+		ESCAPE_CHARS[0x7F] = 1;
+	}
 	
 	Locale locale;
 	int maxDepth;
@@ -91,6 +104,209 @@ class ParseContext {
 			cache.clear();
 		}
 		return cache;
+	}
+	
+	
+	public String parseString(InputSource in) throws IOException {
+		StringCache sc = getCachedBuffer();
+		char start = (char)in.next();
+
+		int rest = in.mark();
+		int len = 0;
+		
+		int n = -1;
+		loop:while ((n = in.next()) != -1) {
+			rest--;
+			len++;
+			
+			char c = (char)n;
+			if (c < ESCAPE_CHARS.length) {
+				switch (ESCAPE_CHARS[c]) {
+				case 1: // control chars
+					throw createParseException(in, "json.parse.UnexpectedChar", c);
+				case 2: // escape chars
+					if (len > 0) in.copy(sc, len - 1);
+					rest = 0;
+					
+					in.back();
+					sc.append(parseEscape(in));
+					break;
+				case 3: // "'
+					if (c == start) {
+						if (len > 0) in.copy(sc, len - 1);
+						break loop;
+					}
+				default:
+					if (rest == 0) in.copy(sc, len);
+				}
+			} else if (c == 0xFEFF) {
+				if (len > 0) in.copy(sc, len - 1);
+				rest = 0;
+			} else {
+				if (rest == 0) in.copy(sc, len);
+			}
+			
+			if (rest == 0) {
+				rest = in.mark();
+				len = 0;
+			}
+		}
+		
+		if (n != start) {
+			throw createParseException(in, "json.parse.StringNotClosedError");
+		}
+		return sc.toString();
+	}
+	
+	public char parseEscape(InputSource in) throws IOException {
+		int point = 1; // 0 '\' 1 'u' 2 'x' 3 'x' 4 'x' 5 'x' E
+		char escape = '\0';
+		
+		int n = in.next();
+		loop:while ((n = in.next()) != -1) {
+			char c = (char)n;
+			if (c == 0xFEFF) continue; // BOM
+			
+			if (point == 1) {
+				switch(c) {
+				case 'b':
+					escape = '\b';
+					break loop;
+				case 'f':
+					escape = '\f';
+					break loop;
+				case 'n':
+					escape = '\n';
+					break loop;
+				case 'r':
+					escape = '\r';
+					break loop;
+				case 't':
+					escape = '\t';
+					break loop;
+				case 'u':
+					point = 2;
+					break;
+				default:
+					escape = c;
+					break loop;
+				}
+			} else {
+				int hex = (c >= '0' && c <= '9') ? c-48 :
+					(c >= 'A' && c <= 'F') ? c-65+10 :
+					(c >= 'a' && c <= 'f') ? c-97+10 : -1;
+				if (hex != -1) {
+					escape |= (hex << ((5-point)*4));
+					if (point != 5) {
+						point++;
+					} else {
+						break loop;
+					}
+				} else {
+					throw createParseException(in, "json.parse.IllegalUnicodeEscape", c);
+				}
+			}
+		}
+		
+		return escape;
+	}
+	
+	public BigDecimal parseNumber(InputSource in) throws IOException {
+		int point = 0; // 0 '(-)' 1 '0' | ('[1-9]' 2 '[0-9]*') 3 '(.)' 4 '[0-9]' 5 '[0-9]*' 6 'e|E' 7 '[+|-]' 8 '[0-9]' 9 '[0-9]*' E
+		StringCache sc = getCachedBuffer();
+		
+		int n = -1;
+		
+		int rest = in.mark();
+		int len = 0;
+		loop:while ((n = in.next()) != -1) {
+			rest--;
+			len++;
+			
+			char c = (char)n;
+			switch(c) {
+			case 0xFEFF: // BOM
+				if (len > 0) in.copy(sc, len - 1);
+				rest = 0;
+				break;
+			case '+':
+				if (point == 7) {
+					if (rest == 0) in.copy(sc, len);
+					point = 8;
+				} else {
+					throw createParseException(in, "json.parse.UnexpectedChar", c);
+				}
+				break;
+			case '-':
+				if (point == 0) {
+					if (rest == 0) in.copy(sc, len);
+					point = 1;
+				} else if (point == 7) {
+					if (rest == 0) in.copy(sc, len);
+					point = 8;
+				} else {
+					throw createParseException(in, "json.parse.UnexpectedChar", c);
+				}
+				break;
+			case '.':
+				if (point == 2 || point == 3) {
+					if (rest == 0) in.copy(sc, len);
+					point = 4;
+				} else {
+					throw createParseException(in, "json.parse.UnexpectedChar", c);
+				}
+				break;
+			case 'e':
+			case 'E':
+				if (point == 2 || point == 3 || point == 5 || point == 6) {
+					if (rest == 0) in.copy(sc, len);
+					point = 7;
+				} else {
+					throw createParseException(in, "json.parse.UnexpectedChar", c);
+				}
+				break;
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				if (point == 0 || point == 1) {
+					if (rest == 0) in.copy(sc, len);
+					point = (c == '0') ? 3 : 2;
+				} else if (point == 2 || point == 5 || point == 9) {
+					if (rest == 0) in.copy(sc, len);
+				} else if (point == 4) {
+					if (rest == 0) in.copy(sc, len);
+					point = 5;
+				} else if (point == 7 || point == 8) {
+					if (rest == 0) in.copy(sc, len);
+					point = 9;
+				} else {
+					throw createParseException(in, "json.parse.UnexpectedChar", c);
+				}
+				break;
+			default:
+				if (point == 2 || point == 3 || point == 5 || point == 6 || point == 9) {
+					if (len > 0) in.copy(sc, len - 1);
+					in.back();
+					break loop;
+				} else {
+					throw createParseException(in, "json.parse.UnexpectedChar", c);
+				}
+			}
+			
+			if (rest == 0) {
+				rest = in.mark();
+				len = 0;
+			}
+		}
+		
+		return sc.toBigDecimal();
 	}
 	
 	public JSONException createParseException(InputSource in, String id) {
--- jsonic-2.0/src/net/arnx/jsonic/internal/parser/StrictJSONParser.java
+++ jsonic-2.0/src/net/arnx/jsonic/internal/parser/StrictJSONParser.java
@@ -1,11 +1,9 @@
 package net.arnx.jsonic.internal.parser;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.Locale;
 
 import net.arnx.jsonic.internal.io.InputSource;
-import net.arnx.jsonic.internal.io.StringCache;
 
 public class StrictJSONParser implements JSONParser {
 	private static final int BEFORE_ROOT = 0;
@@ -14,17 +12,6 @@ public class StrictJSONParser implements JSONParser {
 	private static final int AFTER_NAME = 3;
 	private static final int BEFORE_VALUE = 4;
 	private static final int AFTER_VALUE = 5;
-	
-	private static final int[] ESCAPE_CHARS = new int[128];
-	
-	static {
-		for (int i = 0; i < 32; i++) {
-			ESCAPE_CHARS[i] = 1;
-		}
-		ESCAPE_CHARS['"'] = 2;
-		ESCAPE_CHARS['\\'] = 3;
-		ESCAPE_CHARS[0x7F] = 1;
-	}
 	
 	private int state = BEFORE_ROOT;
 	private InputSource in;
@@ -95,7 +82,7 @@ public class StrictJSONParser implements JSONParser {
 		int n = skip(in, context);
 		if (n == '"') {
 			in.back();
-			context.set(JSONEventType.NAME, parseString(in, context));
+			context.set(JSONEventType.NAME, context.parseString(in));
 			return AFTER_NAME;					
 		} else if (n == '}' && context.getPrevType() == JSONEventType.BEGIN_OBJECT) {
 			context.pop();
@@ -134,7 +121,7 @@ public class StrictJSONParser implements JSONParser {
 				return BEFORE_VALUE;
 			case '"':
 				in.back();
-				context.set(JSONEventType.STRING, parseString(in, context));
+				context.set(JSONEventType.STRING, context.parseString(in));
 				return AFTER_VALUE;
 			case '-':
 			case '0':
@@ -148,7 +135,7 @@ public class StrictJSONParser implements JSONParser {
 			case '8':
 			case '9':
 				in.back();
-				context.set(JSONEventType.NUMBER, parseNumber(in, context));
+				context.set(JSONEventType.NUMBER, context.parseNumber(in));
 				return AFTER_VALUE;	
 			case 't':
 				in.back();
@@ -249,205 +236,6 @@ public class StrictJSONParser implements JSONParser {
 			}
 		}
 		return n;
-	}
-	
-	private static String parseString(InputSource in, ParseContext context) throws IOException {
-		StringCache sc = context.getCachedBuffer();
-		
-		int n = in.next();
-
-		int rest = in.mark();
-		int len = 0;
-		loop:while ((n = in.next()) != -1) {
-			rest--;
-			len++;
-			
-			char c = (char)n;
-			if (c < ESCAPE_CHARS.length) {
-				switch (ESCAPE_CHARS[c]) {
-				case 1: // control chars
-					throw context.createParseException(in, "json.parse.UnexpectedChar", c);
-				case 2: // "
-					if (len > 0) in.copy(sc, len - 1);
-					break loop;						
-				case 3: // escape chars
-					if (len > 0) in.copy(sc, len - 1);
-					rest = 0;
-					
-					in.back();
-					sc.append(parseEscape(in, context));
-					break;
-				default:
-					if (rest == 0) in.copy(sc, len);
-				}
-			} else if (c == 0xFEFF) {
-				if (len > 0) in.copy(sc, len - 1);
-				rest = 0;
-			} else {
-				if (rest == 0) in.copy(sc, len);
-			}
-			
-			if (rest == 0) {
-				rest = in.mark();
-				len = 0;
-			}
-		}
-		
-		if (n != '\"') {
-			throw context.createParseException(in, "json.parse.StringNotClosedError");
-		}
-		return sc.toString();
-	}
-	
-	private static char parseEscape(InputSource in, ParseContext context) throws IOException {
-		int point = 1; // 0 '\' 1 'u' 2 'x' 3 'x' 4 'x' 5 'x' E
-		char escape = '\0';
-		
-		int n = in.next();
-		loop:while ((n = in.next()) != -1) {
-			char c = (char)n;
-			if (c == 0xFEFF) continue; // BOM
-			
-			if (point == 1) {
-				switch(c) {
-				case 'b':
-					escape = '\b';
-					break loop;
-				case 'f':
-					escape = '\f';
-					break loop;
-				case 'n':
-					escape = '\n';
-					break loop;
-				case 'r':
-					escape = '\r';
-					break loop;
-				case 't':
-					escape = '\t';
-					break loop;
-				case 'u':
-					point = 2;
-					break;
-				default:
-					escape = c;
-					break loop;
-				}
-			} else {
-				int hex = (c >= '0' && c <= '9') ? c-48 :
-					(c >= 'A' && c <= 'F') ? c-65+10 :
-					(c >= 'a' && c <= 'f') ? c-97+10 : -1;
-				if (hex != -1) {
-					escape |= (hex << ((5-point)*4));
-					if (point != 5) {
-						point++;
-					} else {
-						break loop;
-					}
-				} else {
-					throw context.createParseException(in, "json.parse.IllegalUnicodeEscape", c);
-				}
-			}
-		}
-		
-		return escape;
-	}
-	
-	private static BigDecimal parseNumber(InputSource in, ParseContext context) throws IOException {
-		int point = 0; // 0 '(-)' 1 '0' | ('[1-9]' 2 '[0-9]*') 3 '(.)' 4 '[0-9]' 5 '[0-9]*' 6 'e|E' 7 '[+|-]' 8 '[0-9]' 9 '[0-9]*' E
-		StringCache sc = context.getCachedBuffer();
-		
-		int n = -1;
-		
-		int rest = in.mark();
-		int len = 0;
-		loop:while ((n = in.next()) != -1) {
-			rest--;
-			len++;
-			
-			char c = (char)n;
-			switch(c) {
-			case 0xFEFF: // BOM
-				if (len > 0) in.copy(sc, len - 1);
-				rest = 0;
-				break;
-			case '+':
-				if (point == 7) {
-					if (rest == 0) in.copy(sc, len);
-					point = 8;
-				} else {
-					throw context.createParseException(in, "json.parse.UnexpectedChar", c);
-				}
-				break;
-			case '-':
-				if (point == 0) {
-					if (rest == 0) in.copy(sc, len);
-					point = 1;
-				} else if (point == 7) {
-					if (rest == 0) in.copy(sc, len);
-					point = 8;
-				} else {
-					throw context.createParseException(in, "json.parse.UnexpectedChar", c);
-				}
-				break;
-			case '.':
-				if (point == 2 || point == 3) {
-					if (rest == 0) in.copy(sc, len);
-					point = 4;
-				} else {
-					throw context.createParseException(in, "json.parse.UnexpectedChar", c);
-				}
-				break;
-			case 'e':
-			case 'E':
-				if (point == 2 || point == 3 || point == 5 || point == 6) {
-					if (rest == 0) in.copy(sc, len);
-					point = 7;
-				} else {
-					throw context.createParseException(in, "json.parse.UnexpectedChar", c);
-				}
-				break;
-			case '0':
-			case '1':
-			case '2':
-			case '3':
-			case '4':
-			case '5':
-			case '6':
-			case '7':
-			case '8':
-			case '9':
-				if (point == 0 || point == 1) {
-					if (rest == 0) in.copy(sc, len);
-					point = (c == '0') ? 3 : 2;
-				} else if (point == 2 || point == 5 || point == 9) {
-					if (rest == 0) in.copy(sc, len);
-				} else if (point == 4) {
-					if (rest == 0) in.copy(sc, len);
-					point = 5;
-				} else if (point == 7 || point == 8) {
-					if (rest == 0) in.copy(sc, len);
-					point = 9;
-				} else {
-					throw context.createParseException(in, "json.parse.UnexpectedChar", c);
-				}
-				break;
-			default:
-				if (point == 2 || point == 3 || point == 5 || point == 6 || point == 9) {
-					if (len > 0) in.copy(sc, len - 1);
-					in.back();
-					break loop;
-				} else {
-					throw context.createParseException(in, "json.parse.UnexpectedChar", c);
-				}
-			}
-			
-			if (rest == 0) {
-				rest = in.mark();
-				len = 0;
-			}
-		}
-		
-		return sc.toBigDecimal();
 	}
 	
 	private static Object parseLiteral(InputSource in, ParseContext context, String literal, Object result) throws IOException {
