9132a8efbbd42f9f0067d11c3d73fa0548acdeb7
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2012-02-05 11:02:31 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@1654 a27f3287-6b31-0410-ac9d-9e0373bb86a5

--- jsonic-1.3/test/net/arnx/jsonic/JSONReaderTest.java
+++ jsonic-1.3/test/net/arnx/jsonic/JSONReaderTest.java
@@ -65,6 +65,36 @@ public class JSONReaderTest {
 		
 		list.clear();
 		
+		reader = json.getReader("[{\"value\": \"a\"}, {\"value\": \"b\", \"child\": {\"value\": \"b1\"} }, {\"value\": \"c\"}]");
+		while ((type = reader.next()) != null) {
+			switch (type) {
+			case START_OBJECT:
+				list.add(reader.getValue(ReaderBean.class));
+				break;
+			}
+		}
+		assertEquals(3, list.size());
+		assertEquals(new ReaderBean("a", null), list.get(0));
+		assertEquals(new ReaderBean("b", new ReaderBean("b1", null)), list.get(1));
+		assertEquals(new ReaderBean("c", null), list.get(2));
+		
+		list.clear();
+		
+		reader = json.getReader("{\"value\": \"a\"}\n{\"value\": \"b\", \"child\": {\"value\": \"b1\"} }\n{\"value\": \"c\"}");
+		while ((type = reader.next()) != null) {
+			switch (type) {
+			case START_OBJECT:
+				list.add(reader.getValue(ReaderBean.class));
+				break;
+			}
+		}
+		assertEquals(3, list.size());
+		assertEquals(new ReaderBean("a", null), list.get(0));
+		assertEquals(new ReaderBean("b", new ReaderBean("b1", null)), list.get(1));
+		assertEquals(new ReaderBean("c", null), list.get(2));
+		
+		list.clear();
+		
 		reader = json.getReader("[1, 2, 3]\n{ \"name\" : \"value\" }\n[true, false, null]");
 		while ((type = reader.next()) != null) {
 			switch (type) {
@@ -94,5 +124,55 @@ public class JSONReaderTest {
 		assertEquals(Boolean.TRUE, list.get(5));
 		assertEquals(Boolean.FALSE, list.get(6));
 		assertNull(list.get(7));
+	}
+}
+
+class ReaderBean {
+	public ReaderBean() {
+	}
+	
+	public ReaderBean(String value, ReaderBean child) {
+		this.value = value;
+		this.child = child;
+	}
+	
+	public String value;
+	
+	public ReaderBean child;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((child == null) ? 0 : child.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ReaderBean other = (ReaderBean) obj;
+		if (child == null) {
+			if (other.child != null)
+				return false;
+		} else if (!child.equals(other.child))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ReaderBean [value=" + value + ", child=" + child + "]";
 	}
 }
