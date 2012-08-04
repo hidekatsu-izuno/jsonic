91d2af22798a8f49b89286669b94eee0798917ad
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2012-08-04 07:28:25 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@1704 a27f3287-6b31-0410-ac9d-9e0373bb86a5

--- jsonic-2.0/src/net/arnx/jsonic/internal/formatter/Formatter.java
+++
@@ -1,9 +0,0 @@
-package net.arnx.jsonic.internal.formatter;
-
-import net.arnx.jsonic.JSON.Context;
-import net.arnx.jsonic.internal.io.OutputSource;
-
-public interface Formatter {
-	boolean format(Context context, Object src, Object o, OutputSource out)
-			throws Exception;
-}
