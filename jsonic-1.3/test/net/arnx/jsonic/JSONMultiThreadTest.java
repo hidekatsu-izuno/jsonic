b85cd3e4606ac65681dddbe486955c78efc0e3cb
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2014-03-08 16:08:35 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@1790 a27f3287-6b31-0410-ac9d-9e0373bb86a5

--- jsonic-1.3/test/net/arnx/jsonic/JSONMultiThreadTest.java
+++ jsonic-1.3/test/net/arnx/jsonic/JSONMultiThreadTest.java
@@ -1,9 +1,5 @@
 package net.arnx.jsonic;
 
-import static org.junit.Assert.*;
-
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -15,27 +11,7 @@ import net.arnx.jsonic.JSONTest.JSONTester;
 
 import org.junit.Test;
 
-public class JSONMultiThreadTest {
-	@Test
-	public void testClassLoader() throws Exception {
-		ClassLoader cl = new URLClassLoader(new URL[] {
-				new URL("file://./lib/jsp-api.jar")
-		});
-		
-		ClassLoader backup = Thread.currentThread().getContextClassLoader();
-		try {
-			Thread.currentThread().setContextClassLoader(cl);
-			
-			List<String> list = new ArrayList<String>();
-			
-			assertEquals("[]", JSON.encode(list));
-			assertEquals(list, JSON.decode("[]"));
-			
-		} finally {
-			Thread.currentThread().setContextClassLoader(backup);
-		}
-	}
-	
+public class JSONMultiThreadTest {	
 	@Test
 	public void testMultiThread() throws Exception {
 		ExecutorService service = Executors.newFixedThreadPool(50);
