ce8a1b2175e4de7fde6fc4d10f595e0754e93593
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2014-04-26 01:54:27 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@1803 a27f3287-6b31-0410-ac9d-9e0373bb86a5

--- jsonic-1.3/src/net/arnx/jsonic/util/BeanInfo.java
+++ jsonic-1.3/src/net/arnx/jsonic/util/BeanInfo.java
@@ -1,12 +1,12 @@
-/* 
+/*
  * Copyright 2014 Hidekatsu Izuno
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,9 +26,9 @@ import java.util.Map;
 import java.util.WeakHashMap;
 
 public final class BeanInfo {
-	private static final Map<ClassLoader, Map<Class<?>, BeanInfo>> cache = 
+	private static final Map<ClassLoader, Map<Class<?>, BeanInfo>> cache =
 		new WeakHashMap<ClassLoader, Map<Class<?>, BeanInfo>>();
-	
+
 	public static BeanInfo get(Class<?> cls) {
 		synchronized(cache) {
 			BeanInfo info = null;
@@ -43,7 +43,7 @@ public final class BeanInfo {
 			} else {
 				info = map.get(cls);
 			}
-			
+
 			if (info == null) {
 				info = new BeanInfo(cls);
 				map.put(cls, info);
@@ -51,49 +51,49 @@ public final class BeanInfo {
 			return info;
 		}
 	}
-	
+
 	public static void clear() {
 		synchronized (cache) {
 			cache.clear();
 		}
 	}
-	
+
 	private Class<?> type;
 	private ConstructorInfo ci;
 	private Map<String, PropertyInfo> sprops;
 	private Map<String, MethodInfo> smethods;
 	private Map<String, PropertyInfo> props;
 	private Map<String, MethodInfo> methods;
-	
+
 	private BeanInfo(Class<?> cls) {
 		type = cls;
-		
+
 		for (Constructor<?> con : cls.getConstructors()) {
 			if (con.isSynthetic()) {
 				continue;
 			}
-			
+
 			if (ci == null) {
 				ci = new ConstructorInfo(cls, null);
 			}
 			con.setAccessible(true);
-			ci.constructors.add(con);			
+			ci.constructors.add(con);
 		}
-		
+
 		for (Field f : cls.getFields()) {
-			if (f.isSynthetic()) {
+			if (f.isSynthetic() || ClassLoader.class.isAssignableFrom(f.getType())) {
 				continue;
 			}
-			
+
 			boolean isStatic = Modifier.isStatic(f.getModifiers());
-			
+
 			String name = f.getName();
 			f.setAccessible(true);
 			if (isStatic) {
 				if (sprops == null) {
 					sprops = Collections.synchronizedMap(new LinkedHashMap<String, PropertyInfo>());
 				}
-				sprops.put(name, new PropertyInfo(cls, name, f, null, null, isStatic, -1));				
+				sprops.put(name, new PropertyInfo(cls, name, f, null, null, isStatic, -1));
 			} else {
 				if (props == null) {
 					props = Collections.synchronizedMap(new LinkedHashMap<String, PropertyInfo>());
@@ -101,18 +101,18 @@ public final class BeanInfo {
 				props.put(name, new PropertyInfo(cls, name, f, null, null, isStatic, -1));
 			}
 		}
-		
+
 		for (Method m : cls.getMethods()) {
 			if (m.isSynthetic() || m.isBridge()) {
 				continue;
 			}
-			
+
 			String name = m.getName();
 			Class<?>[] paramTypes = m.getParameterTypes();
 			Class<?> returnType = m.getReturnType();
-			
+
 			boolean isStatic = Modifier.isStatic(m.getModifiers());
-			
+
 			MethodInfo mi;
 			if (isStatic) {
 				if (smethods == null) {
@@ -135,26 +135,37 @@ public final class BeanInfo {
 			}
 			m.setAccessible(true);
 			mi.methods.add(m);
-			
+
 			boolean isReadMethod;
 			int start = 0;
-			if (name.startsWith("get")
-					&& paramTypes.length == 0 && !returnType.equals(void.class)) {
+			if (name.startsWith("get")) {
 				isReadMethod = true;
 				start = 3;
-			} else if (name.startsWith("is") 
-					&& paramTypes.length == 0 && !returnType.equals(void.class)) {
+			} else if (name.startsWith("is")) {
 				isReadMethod = true;
 				start = 2;
-			} else if (name.startsWith("set")
-					&& paramTypes.length == 1 && !paramTypes[0].equals(void.class)) {
+			} else if (name.startsWith("set")) {
 				isReadMethod = false;
 				start = 3;
 			} else {
 				continue;
 			}
-			
-			if ((name.length() > start && !Character.isLowerCase(name.charAt(start))) 
+
+			if (isReadMethod) {
+				if (paramTypes.length != 0
+						|| returnType.equals(void.class)
+						|| ClassLoader.class.isAssignableFrom(returnType)) {
+					continue;
+				}
+			} else {
+				if (paramTypes.length != 1
+						|| paramTypes[0].equals(void.class)
+						|| ClassLoader.class.isAssignableFrom(paramTypes[0])) {
+					continue;
+				}
+			}
+
+			if ((name.length() > start && !Character.isLowerCase(name.charAt(start)))
 					|| (name.length() > start + 1 && !Character.isLowerCase(name.charAt(start + 1)))) {
 				name = name.substring(start);
 				if (name.length() < 2 || !Character.isUpperCase(name.charAt(1))){
@@ -165,7 +176,7 @@ public final class BeanInfo {
 			} else {
 				continue;
 			}
-			
+
 			PropertyInfo prop;
 			if (isStatic) {
 				if (sprops == null) {
@@ -186,20 +197,20 @@ public final class BeanInfo {
 					props.put(name, prop);
 				}
 			}
-			
+
 			if (isReadMethod) {
 				prop.readMethod = m;
 			} else {
 				prop.writeMethod = m;
 			}
 		}
-		
+
 		if (sprops == null) sprops = Collections.emptyMap();
 		if (smethods == null) smethods = Collections.emptyMap();
 		if (props == null) props = Collections.emptyMap();
 		if (methods == null) methods = Collections.emptyMap();
 	}
-	
+
 	public Object newInstance() {
 		try {
 			Constructor<?> target = type.getConstructor();
@@ -209,43 +220,43 @@ public final class BeanInfo {
 			throw new IllegalStateException(e);
 		}
 	}
-	
+
 	public Class<?> getType() {
 		return type;
 	}
-	
+
 	public ConstructorInfo getConstructor() {
 		return ci;
 	}
-	
+
 	public PropertyInfo getStaticProperty(String name) {
 		return sprops.get(name);
 	}
-	
+
 	public MethodInfo getStaticMethod(String name) {
 		return smethods.get(name);
 	}
-	
+
 	public Collection<PropertyInfo> getStaticProperties() {
 		return sprops.values();
 	}
-	
+
 	public Collection<MethodInfo> getStaticMethods() {
 		return smethods.values();
 	}
-	
+
 	public PropertyInfo getProperty(String name) {
 		return props.get(name);
 	}
-	
+
 	public MethodInfo getMethod(String name) {
 		return methods.get(name);
 	}
-	
+
 	public Collection<PropertyInfo> getProperties() {
 		return props.values();
 	}
-	
+
 	public Collection<MethodInfo> getMethods() {
 		return methods.values();
 	}
@@ -275,11 +286,11 @@ public final class BeanInfo {
 	@Override
 	public String toString() {
 		return "BeanInfo [static properties = " + sprops
-			+ ", static methods = " + smethods 
+			+ ", static methods = " + smethods
 			+ ", properties = " + props
 			+ ", methods = " + methods + "]";
 	}
-	
+
 	static int calcurateDistance(Class<?>[] params, Object[] args) {
 		int point = 0;
 		for (int i = 0; i < args.length; i++) {
