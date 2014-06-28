6f7c8f74b1935167720412ed85064372422bd090
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2014-06-28 08:52:03 +0000

    git-svn-id: https://svn.osdn.net/svnroot/jsonic/trunk@1828 a27f3287-6b31-0410-ac9d-9e0373bb86a5

--- jsonic-1.3/test/net/arnx/jsonic/JSONMultiThreadTest.java
+++ jsonic-1.3/test/net/arnx/jsonic/JSONMultiThreadTest.java
@@ -3,28 +3,31 @@ package net.arnx.jsonic;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import net.arnx.jsonic.JSONTest.JSONTester;
 
 import org.junit.Test;
 
-public class JSONMultiThreadTest {	
+public class JSONMultiThreadTest {
 	@Test
 	public void testMultiThread() throws Exception {
-		ExecutorService service = Executors.newFixedThreadPool(50);
-		
+		ExecutorService service = new ThreadPoolExecutor(50, 50,
+				0L, TimeUnit.MILLISECONDS,
+				new LinkedBlockingQueue<Runnable>());
+
 		List<JSONTester> list = new ArrayList<JSONTester>();
 		for (int i = 0; i < 1000; i++) {
 			list.add(new JSONTester());
 		}
 		List<Future<Object>> results = service.invokeAll(list);
-		
+
 		service.shutdown();
-		service.awaitTermination(1, TimeUnit.MINUTES);
-		
+		service.awaitTermination(60 * 1000, TimeUnit.MILLISECONDS);
+
 		for (Future<Object> future : results) {
 			future.get();
 		}
